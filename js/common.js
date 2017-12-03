8736d50d30907803345b9e509b2e9638102b6d54
Anna Korotkova <ania.korotkovafd@gmail.com>
2017-12-03 12:15:22 +0200

    03.12

--- js/common.js
+++ js/common.js
@@ -22,3 +22,31 @@ $(window).load(function() {
 
 });
 
+/*document.addEventListener('DOMContentLoaded', function(){
+
+	'use srtict';
+
+	var header = document.querySelector('.header').scrollHeight;
+	var nav_block = document.querySelector('.nav_block').scrollHeight;
+	
+	var full_height = header + 120;
+
+	window.onscroll = function() {
+
+  		var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+
+			if ( scrolled > full_height) {
+
+				document.querySelector('.nav_block').classList.add('fixed');
+			
+			} else {
+
+				document.querySelector('.nav_block').classList.remove('fixed');
+			}
+	}
+
+	
+
+
+})
+*/
